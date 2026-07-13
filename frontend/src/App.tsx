972ec2f613d22368e35c361241750e0768930091
MarcusVRDN <marcusvinirdn@gmail.com>
2026-07-13 20:00:30 -0300

    feat: initialize React frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-white">
+          Sistema de Controle de Produção
+        </h1>
+
+        <p className="mt-4 text-slate-300">
+          Meu primeiro projeto com React + Tailwind
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
