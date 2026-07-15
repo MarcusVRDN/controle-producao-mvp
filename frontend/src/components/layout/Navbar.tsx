bdb2080c7a8142e4fd7167220bbfd4387841752e
MarcusVRDN <marcusvinirdn@gmail.com>
2026-07-14 23:49:59 -0300

    feat: cria tela de clientes no frontend

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <header className="flex h-16 bg-slate-900 items-center justify-between border-b border-slate-700 bg-slate-900 px-6">
+    <header className="flex h-16 bg-slate-900 items-center justify-between border-b border-slate-700 px-6">
         <h1 className="text-lg font-semibold text-white">Controle de Produção</h1>
         <span className="text-sm text-slate-300"> Usuário</span>
     </header>
