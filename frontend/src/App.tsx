9ca84fc55c0efd2030e41da7cceea0cdcbfbf9f8
MarcusVRDN <marcusvinirdn@gmail.com>
2026-07-22 23:32:04 -0300

    feat: implementa rotas do frontend com react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Clientes from "./pages/Clientes";
 import Pecas from "./pages/Pecas";
 import Pedidos from "./pages/Pedidos";
+import OrdensServico from "./pages/OrdensServico";
 
 function App() {
   return (
-    <Layout>
-      <Pecas />
-    </Layout>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Navigate to= "/clientes" replace/>}/>
+        <Route path="/clientes" element={<Clientes />}/>
+        <Route path="/pecas" element={<Pecas />} />
+        <Route path="/pedidos" element={<Pedidos/>} />
+        <Route path="/ordens-servico" element={<OrdensServico/>}/>
+      </Route>
+    </Routes>
   );
 }
 
