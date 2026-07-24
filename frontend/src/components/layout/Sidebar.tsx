efc659a526f6aa19c86383047d931e0b60c51c9b
MarcusVRDN <marcusvinirdn@gmail.com>
2026-07-23 21:38:56 -0300

    feat: implementa navegacao das rotas pela sidebar

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -1,21 +1,51 @@
+import { NavLink } from "react-router-dom";
+
 function Sidebar() {
-    const menuItems = ["Dashboard", "Clientes", "Peças", "Pedidos", "Ordens de Serviço"]
+  const menuItems = [
+    {
+      nome: "Clientes",
+      caminho: "/clientes",
+    },
+    {
+      nome: "Peças",
+      caminho: "/pecas",
+    },
+    {
+      nome: "Pedidos",
+      caminho: "/pedidos",
+    },
+    {
+      nome: "Ordens de Serviço",
+      caminho: "/ordens-servico",
+    },
+  ];
   return (
     <aside className="h-screen w-64 bg-slate-950 p-6">
-        <h1 className="text-center p-6 text-lg font-semibold text-white">Controle de Produção</h1>
-        <nav>
-            <ul className="space-y-2 text-white">
-                {menuItems.map((texto) => (
-                    <li 
-                    key = {texto}
-                    className= "rounded px-4 py-2 hover:bg-slate-800">
-                        {texto}
-                    </li>
-                ))}
-            </ul>
-        </nav>
+      <h1 className="text-center p-6 text-lg font-semibold text-white">
+        Controle de Produção
+      </h1>
+      <nav>
+        <ul className="space-y-2 text-white">
+          {menuItems.map((item) => (
+            <li key={item.nome}>
+              <NavLink
+                to={item.caminho}
+                className={({ isActive }) =>
+                  `flex rounded-lg px-4 py-3 transition ${
+                    isActive
+                      ? "bg-slate-800 text-white"
+                      : "text-slate-300 hover:bg-slate-900"
+                  }`
+                }
+              >
+                {item.nome}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </aside>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
